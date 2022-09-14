7e6ade20d1dc554ad21647f184c2bcab1ea3dd24
Askhab Magomedov <asmag86@gmail.com>
2022-09-14 17:15:36 +0300

    add files for section 2.3

---
+++ 002 Data Input-Output/2.3 print() command - sep & end parameters/#2.3-main.py
@@ -0,0 +1,22 @@
+print('a', 'b', 'c')
+print('d', 'e', 'f')
+
+# additional parameter - sep
+print('a', 'b', 'c', sep='*')
+print('e', 'd', 'f', sep='**')
+
+# additional parameter - end
+print('a', 'b', 'c', end='@')
+print('d', 'e', 'f', end='@@')
+print()
+
+# sep & end - both
+print('a', 'b', 'c', sep='*', end='finish')
+print('d', 'e', 'f', sep='**', end='^__^')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='#')
+print('m', 'n', 'o', sep='/', end='!')
+print()
+
+# removing all additional symbols
+print('a', 'b', 'c', sep='', end='')
