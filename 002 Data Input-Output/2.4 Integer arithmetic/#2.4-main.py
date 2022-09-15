86fd2c6bce073207e1d3928aee669055d9d96f03
Askhab Magomedov <asmag86@gmail.com>
2022-09-15 12:03:57 +0300

    2.4 main file end

---
+++ 002 Data Input-Output/2.4 Integer arithmetic/#2.4-main.py
@@ -0,0 +1,50 @@
+# whole numbers
+num1 = 7
+num2 = 10
+num3 = num1 + num2
+print(num3)
+
+# Basic operations with numbers
+# + addition
+# - subtraction
+# * multiplication
+# / division
+
+a = 3
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Order of operations
+num1 = 2 + 3 * 4
+# brackets changed priority of operations
+num2 = (2 + 3) * 4
+print(num1)
+print(num2)
+
+# Variable type conversion
+s = '1992'
+print(s)
+year = int(s)
+print(year)
+
+# input() always reads text in variable
+num1 = input()
+num2 = input()
+print(num1 + num2)
+
+# to receive numbers to variable use - num()
+num1 = int(input())
+num2 = int(input())
+print(num1 + num2)
+
+# Converting integer into string
+num = 17
+s = str(17)
+
+# unary minus
+num1 = -6
+# binary minus
+num2 = 17 - 7
