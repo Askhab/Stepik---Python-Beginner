4c6c77a397f35f366023f4145e48438e1f204822
Askhab Magomedov <asmag86@gmail.com>
2022-11-16 09:46:05 +0300

    end section 15.7

--- 015 Work on mini-project/15.7 Word guessing/#15.7-exercise.py
+++ 015 Work on mini-project/15.7 Word guessing/#15.7-exercise.py
@@ -1,21 +1,25 @@
 from random import choice
 
-word_list = ["человек", "слово", "лицо", "дверь", "земля", "корабль", "ребенок", "история", "женщина", "развитие",
-             "пщеница", "хозяйство", "поселение", "спектакль", "автомобиль", "экономика", "литература", "граница",
-             "магазин", "заграница", "ячмень", "республика", "личность"]
+en = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+word_list = ['air', 'animal', 'answer', 'area', 'bird', 'body', 'book', 'bottom', 'boy', 'brother', 'car', 'child', 'children', 'city', 'class', 'color', 'country', 'day', 'dog', 'door', 'east', 'example', 'eye', 'face', 'family', 'farm', 'father', 'feet', 'fire', 'fish', 'food', 'foot', 'friend', 'girl', 'hand', 'head', 'home', 'horse', 'house', 'idea', 'king', 'land', 'letter', 'life', 'line', 'list', 'love', 'men', 'money', 'month', 'mother', 'mountain', 'name', 'night', 'north', 'number', 'order', 'page', 'paper', 'pen', 'people', 'person', 'phone', 'picture', 'piece', 'place', 'plant', 'problem', 'product', 'question', 'river', 'rock', 'room', 'school', 'science', 'sea', 'sentence', 'ship', 'side', 'sister', 'song', 'sound', 'south', 'space', 'state', 'story', 'thriller', 'top', 'tree', 'watch', 'water', 'week', 'west', 'wind', 'woman', 'women', 'wood', 'word', 'world', 'year']
 
 
-def get_word(words):
-    return choice(words).upper()
-
-
-def letter_in_word(letter, word):
-    output = ""
+def check_input(word, alphabet):
+    flag = False
     for i in word:
-        if i == letter:
+        if alphabet.find(i) != -1:
+            flag = False
+        else:
+            flag = True
+
+
+def letter_in_word(letter, word, hidden):
+    output = ""
+    for i in range(len(word)):
+        if word[i] == letter:
             output += letter
         else:
-            output += "_"
+            output += hidden[i]
 
     return output
 
@@ -88,9 +92,6 @@ def display_hangman(tries):
     return stages[tries]
 
 
-chosen_word = get_word(word_list)
-
-
 def play(word):
     word_completion = '_' * len(word)  # строка, содержащая символы _ на каждую букву задуманного слова
     guessed = False  # сигнальная метка
@@ -104,39 +105,57 @@ def play(word):
 
     print(f"THE WORD IS - {word}")
 
-    while tries < 6 or not guessed:
+    while tries > 0:
         try_word = input("Enter your word or letter! ").upper()
 
-        if not try_word.isalpha() or len(try_word) < 1:
-            print("Please enter only english alphabetical letters and try again!")
+        if len(try_word) < 1 or check_input(try_word, en):
+            print("Please enter only english alphabetical letter or word and try again!")
             continue
 
         if try_word in guessed_words or try_word in guessed_letters:
             print("You already tried this. This try didn't count.")
             continue
 
-        if len(try_word) == 1 and try_word in word and not guessed:
-            guessed_letters.append(try_word)
-            tries -= 1
+        if len(try_word) == 1:
+            if try_word in word:
+                guessed_letters.append(try_word)
+                tries -= 1
 
-            print(f"You're guessed letter. But didn't guessed word. You have {tries} tries left.")
-            print(f"Your progress is {letter_in_word(try_word, word)}")
-            print(display_hangman(tries))
-        else:
-            tries -= 1
-            print(f"The entered letter is not in the guessed word. You have {tries} tries left.")
-            print(display_hangman(tries))
+                print(display_hangman(tries))
+                print(f"You're guessed letter. But didn't guessed word. You have {tries} tries left.")
+                word_completion = letter_in_word(try_word, word, word_completion)
+                print(f"Your progress is {word_completion}")
+            else:
+                tries -= 1
+                print(display_hangman(tries))
+                print(f"The entered letter is not in the guessed word. You have {tries} tries left.")
 
-        if try_word == word:
-            print("Congratulations, you guessed the word! You won!")
-        else:
-            tries -= 1
-            guessed_words.append(try_word)
-            print(f"You didn't guessed the word. You have {tries} tries left. Try again.")
+        if len(try_word) > 1:
+            if try_word == word:
+                print("Congratulations, you guessed the word! You won!")
+
+                answer = input("Would you like to play again? Enter - yes, or - no: ").upper()
+
+                if answer == "YES":
+                    play(chosen_word.upper())
+                elif answer == "NO":
+                    break
+            else:
+                tries -= 1
+                guessed_words.append(try_word)
+                print(display_hangman(tries))
+                print(f"You didn't guessed the word. You have {tries} tries left. Try again.")
     else:
         print(display_hangman(tries))
         print("You failed to guess the word.")
         print(f"The word was - {word}")
 
+        answer = input("Would you like to play again? Enter - yes, or - no: ").upper()
 
-play(chosen_word.upper())
+        if answer == "YES":
+            play(choice(word_list).upper())
+        elif answer == "NO":
+            return False
+
+
+play(choice(word_list).upper())
